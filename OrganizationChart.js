97b172d764c521b14fa0da80f40370449bba0e3e
Yusuf Alp <alpyusuf007@gmail.com>
2020-11-05 09:22:08 -0800

    Solving a problem

---
+++ OrganizationChart.js
@@ -0,0 +1,20 @@
+const chart = {
+  "Zuckerberg": {
+    "Schroepfer": {
+      "Bosworth": ["Steve", "Kyle", "Andra"],
+      "Zhao": ["Richie", "Sofia", "Jen"]
+    },
+    "Schrage": {
+      "VanDyck": [, "Sabrina", "Michelle", "Josh"],
+      "Swain": ["Blanch", "Tom", "Joe"]
+    },
+    "Sandberg": {
+      "Goler": ["Eddie", "Julie", "Annie"],
+      "Hernandez": ["Rowi", "Inga", "Morgan"],
+      "Moissinac": ["Amy", "Chuck", "Vinnie"],
+      "Kelley": ["Eric", "Ana", "Wes"]
+    }
+  }
+}
+
+console.log(chart["Zuckerberg"]["Schroepfer"])
